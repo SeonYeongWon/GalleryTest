7ea31ce1f388d033c2f011b880d7c2d20baa4e3b
SeonYeong Won <dnj1981@naver.com>
2016-10-25 20:13:39 +0900

    #3 Event Programming

--- app/src/main/java/kr/hs/emirim/dnj1981/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dnj1981/gallerytest/MainActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.dnj1981.gallerytest;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -44,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
             imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imgV.setPadding(5,5,5,5);
             imgV.setImageResource(posterIDs[position]);
+            final int pos=position;
+            imgV.setOnTouchListener(new View.OnTouchListener(){
+                public boolean onTouch(View v, MotionEvent event){
+                    imgMain.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    imgMain.setImageResource(posterIDs[pos]);
+                    return false;
+                }
+            });
             return imgV;
         }
         public long getItemId(int position){
